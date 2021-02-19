0e944adea35b9a1b251d446184505e31b5930c75
borto <matt.bortoletto@gmail.com>
2021-02-19 17:52:06 +0100

    code cleaned

--- 5_attemptworks/distributed_matrix_diag.F90
+++ 5_attemptworks/distributed_matrix_diag.F90
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 include "modules/debug_module.F90"
 include "modules/matrix_interface.F90"
 include "modules/scalapack_interface.F90"
 include "modules/hamiltonians.F90"
-=======
-#include "modules/debug_module.F90"
-#include "modules/matrix_interface.F90"
-#include "modules/scalapack_interface.F90"
-#include "modules/hamiltonians.F90"
-#include "modules/utils.F90"
+include "modules/utils.F90"
 
->>>>>>> main
 
 program test_scalapack
 
@@ -22,114 +15,88 @@ program test_scalapack
     
 	implicit none
 	
-    !) BLACS
+    ! blacs variables
     integer :: iam, nprocs, nprow, npcol, myrow, mycol, context
-    !)  MATRICES
+    ! system variables
     integer :: N, sizeg
-    PARAMETER (N= 7) 
+    PARAMETER (N=7) 
     PARAMETER (sizeg= 2**N) 
     double complex, dimension(sizeg, sizeg) :: M,H,L
     double precision , dimension(sizeg) :: eigvaltest, w
-    
+    real*8 :: lambda
+    real*8 :: couplings(3)
+    !complex*16 :: testmatrix(2,2)
+    double complex, dimension(N) :: reselx
+    double complex, dimension(2**N) :: statex
+    ! scalapack variables
     integer :: maxn
     PARAMETER  (MAXN = 500)
     integer :: lda, ii
     PARAMETER (lda = maxn)
-    
     double complex, dimension(lda,lda) :: A,Z
     integer, dimension(9) :: desca,  descz
     integer :: info , nb
-    real*8 :: couplings(3)
-    real*8 :: lambda
-    complex*16 :: testmatrix(2,2)
-<<<<<<< HEAD
-
-    couplings = (/1.d0,1.d0,1.d0/)
-    nb = 4
-    lambda = 3.3
-
-=======
-    
-    double complex, dimension(N) :: reselx
-    double complex, dimension(2**N) :: statex
-    
-    
     
     couplings = (/1.d0,3.d0,1.d0/)
     nb = 4
     
-    !1) INITIALIZE BLACS
->>>>>>> main
-	NPROW= 2
-	NPCOL=2
+    ! ---- initialize BLACS -------------------------------------------------------
+	NPROW = 2
+	NPCOL = 2
 	
-	CALL BLACS_PINFO( IAM, NPROCS )
-	IF( ( NPROCS.LT.1 ) ) THEN
-		CALL BLACS_SETUP( IAM, NPROW*NPCOL )
+	CALL BLACS_PINFO(IAM, NPROCS)
+	IF((NPROCS.LT.1)) THEN
+		CALL BLACS_SETUP(IAM, NPROW*NPCOL)
 	END IF
 	
-	CALL BLACS_GET( -1, 0, CONTEXT )
-	CALL BLACS_GRIDINIT( CONTEXT, 'R', NPROW, NPCOL )
-	CALL BLACS_GRIDINFO( CONTEXT, NPROW, NPCOL, MYROW, MYCOL )
-    	
-    	
+	CALL BLACS_GET(-1, 0, CONTEXT)
+	CALL BLACS_GRIDINIT(CONTEXT, 'R', NPROW, NPCOL)
+	CALL BLACS_GRIDINFO(CONTEXT, NPROW, NPCOL, MYROW, MYCOL)
+    		
     IF(MYROW .eq. -1) THEN
-		WRITE(*,*) " ERROR, blacs context not valid "
+		WRITE(*,*) "ERROR, blacs context not valid!"
 		CALL BLACS_EXIT(0) 
 		STOP
-	END IF 
-	if(iam .eq. 0 ) then
-		open(unit = 22, file="data3.txt", action="write" , status="old")
-	end if
-do ii = 1, 40
-    lambda = (ii-20)*(1.d0/19.d0)
+    END IF 
+    ! -----------------------------------------------------------------------------
 
-	
-	
-	A= dcmplx(0.d0,0.d0)
-	
-	call DESCINIT( DESCA, sizeg, sizeg, nb, nb, 0, 0, context, lda, info)
-	call heisenbergmodel_hamiltonian(context, lambda,couplings,N, A, descA)
-	!call transverse_field_ising_model_hamiltonian(context, lambda,N, A, descA)
-   
-    call DESCINIT( DESCZ, sizeg, sizeg, nb, nb, 0, 0, context, lda, info)
+    ! ---- Do the computations ----------------------------------------------------
+	if (iam .eq. 0) then
+		open(unit=22, file="data3.txt", action="write" , status="old")
+    end if
     
-    call ddzm(A,descA, Z, descz, W)
+    do ii = 1, 40
+
+        lambda = (ii-20)*(1.d0/19.d0)
+
+        A= dcmplx(0.d0, 0.d0)
+        
+        call DESCINIT(DESCA, sizeg, sizeg, nb, nb, 0, 0, context, lda, info)
+        call heisenbergmodel_hamiltonian(context, lambda,couplings,N, A, descA)
+        !call transverse_field_ising_model_hamiltonian(context, lambda,N, A, descA)
     
-    !statex = getcol(Z,descZ,1)
-    !call printmat(Z,descZ)
-    IF (IAM .eq. 0 ) then
-<<<<<<< HEAD
-		print*, "EIGENVALUES"      
-		print*, w(1)/(N-1)
-    END IF
+        call DESCINIT(DESCZ, sizeg, sizeg, nb, nb, 0, 0, context, lda, info)
+        
+        call ddzm(A,descA, Z, descz, W)
+        
+        !statex = getcol(Z,descZ,1)
+        !call printmat(Z,descZ)
+        IF (IAM .eq. 0) then
+            !print*, ii 
+            !write(*, ('B64')) maxval()  
+            !print*, statex
+            write (22, *) (ii-20)*dble(1.d0/19.d0), w(1)/(N-1)
+            reselx = getavgvalue(statex, 'y', N)
+            !print*, reselx 
+        END IF
 
-	CALL BLACS_GRIDEXIT( CONTEXT )
-	CALL BLACS_EXIT(0)
-=======
-		!print*, ii 
-		!write(*, ('B64')) maxval()  
-		!print*, statex
-		write(22,*)(ii-20)*dble(1.d0/19.d0), w(1)/((N-1))
-		
-		reselx =  getavgvalue(statex, 'y', N)
-		!print*, reselx
-		
-		
-    END IF
-    
+    end do 
 
-	
-	
-end do 
+    close(22) 
 
-CALL BLACS_GRIDEXIT( CONTEXT )
-	CALL BLACS_EXIT(0)
-	
-		close(22) 
-   
-
- 
->>>>>>> main
+    ! ---- exit BLACS -------------------------------------------------------------
+    CALL BLACS_GRIDEXIT(CONTEXT)
+    CALL BLACS_EXIT(0)
+    ! -----------------------------------------------------------------------------
 
 end program test_scalapack
--- 5_attemptworks/modules/scalapack_interface.F90
+++ 5_attemptworks/modules/scalapack_interface.F90
@@ -2,264 +2,245 @@
 !Module to handle matrices in a distribute fashion
 !#################################################
 module scalapack_interface
-use matrix_interface
-use debug_module
-implicit none
+
+	use matrix_interface
+	use debug_module
+	
+	implicit none
+	
 contains
 
-
-!------------------------------------------------------------------------------------------
-!Compute the sum for distribute matrices, safely.
-subroutine dsum(A, descA, B, descB, C,descC)
-	integer, dimension(:):: descA, descB , descC
-	double complex, dimension(:,:):: A,B,C
-	double complex :: alpha, beta
-	
-	integer ii , jj
-	
-	call breakifn("Wrong dimensions" , all( descA(3:4) .eq. descB(3:4)) , .true.)
-	call breakifn("Wrong dimensions" , all( descA(3:4) .eq. descC(3:4)) , .true.)
-	
-	if(all((descA .eq. descB) .and. (descB .eq. descC) )) then 
-		C = A+B
+	!Computes the sum for distribute matrices, safely.
+	subroutine dsum(A, descA, B, descB, C, descC)
 		
-	!else
-	!	do ii = 1 , descA(3)
-	!		do jj = 1, descA(4)
-	!		call pzelget('A','I',A,ii, jj ,descA, alpha)
-	!		call pzelget('A','I',B,ii, jj ,descB, beta)
-	!		 
-	!		
-	!		call pzelset(C, ii ,jj , descC, alpha + beta)
-	!		end do 
-	!	end do 
-	end if
-	
-
-end subroutine
-
-
-!------------------------------------------------------------------------------------------
-!input M , --> A, desca
-subroutine build_matrix(M,iam, A, desca)
-	use mpi
-	use matrix_interface
-	implicit none
-	
-	!input
-	double complex , dimension(:,:), intent(INOUT) :: M, A
-	integer, intent(INOUT) ::  iam
-	integer, dimension(:), intent(INOUT) ::  desca
-	!vars 
-	integer :: sizeG 
-	integer :: ierr
-	integer :: ii, jj 
-	
-	
-	
-	!Check M
-	if( size(M, dim=1) .eq. size(M, dim = 2)) then 
-		sizeG =   size(M, dim=1)
-	else 
-		print*, "not a square matrix "
-		STOP
-	end if
-	
-
-	
-	!USE M from the first process
-	if(iam .eq. 0) then 
-		print*, "------------------"
-		call MPI_BCAST(m,sizeg*sizeg, MPI_DOUBLE_COMPLEX, 0 ,MPI_COMM_WORLD,ierr)
-	else 
-		call MPI_BCAST(m,sizeg*sizeg, MPI_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD,ierr )
-	end if
+		integer, dimension(:) :: descA, descB, descC
+		double complex, dimension(:,:) :: A, B, C
+		double complex :: alpha, beta
+		integer ii, jj
 		
-	!Print to show matrices
-	!do ii =0 , nprows*npcols
-    	!if(iam .eq. ii) then
-    	!print*, "******"
-    	!call pzm(M)
-    	!end if 
-    	!call mpi_barrier(MPI_COMM_WORLD, ierr) 
-    	!end do
-
+		call breakifn("Wrong dimensions", all( descA(3:4) .eq. descB(3:4)), .true.)
+		call breakifn("Wrong dimensions", all( descA(3:4) .eq. descC(3:4)), .true.)
 		
-	if (ierr .ne. 0 ) then 
-		print*, "error, building matrix "
-		STOP
-	end if
-	
-	!Distribute M 
-	DO ii=1,sizeg
-		DO jj=1,sizeg
-			CALL PZELSET( A, ii, jj, DESCA, M(ii,jj) )	
-		ENDDO
-	ENDDO
+		if (all((descA .eq. descB) .and. (descB .eq. descC))) then 
+			C = A + B	
+		!else
+		!	do ii = 1 , descA(3)
+		!		do jj = 1, descA(4)
+		!		call pzelget('A','I',A,ii, jj ,descA, alpha)
+		!		call pzelget('A','I',B,ii, jj ,descB, beta)
+		!		 
+		!		
+		!		call pzelset(C, ii ,jj , descC, alpha + beta)
+		!		end do 
+		!	end do 
+		end if
 		
-	
-end subroutine 
+	end subroutine
 
-!------------------------------------------------------------------------------------------
-!simple interface to print distributed matrix
-subroutine printmat(A, descA)
-	integer , dimension(:):: desca 
-	double complex, dimensioN(:,:):: A
-	double complex, dimension(30) :: work
-	CALL PZLAPRNT( descA(3),descA(4), A, 1, 1, DESCA, 0, 0, 'A', 6, WORK )
-end subroutine 
+	! Distributed matrix builder
+	subroutine build_matrix(M, iam, A, desca)
 
-!------------------------------------------------------------------------------------------
-!diagonalize matrix
-subroutine ddzm(A, desca, Z, descz, W)
-        
-        use matrix_interface
-        implicit none
-        
-        !input 
-        double complex , dimension(:,:), intent(INOUT) :: A, Z
-        integer , dimension(:), intent(INOUT):: desca, descz
-        double precision, dimension(:), intent(INOUT)::W
-        double complex, dimension(:), allocatable ::  rwork
-        integer :: lwork, lrwork
-        integer :: info
-        double precision, dimension(:), allocatable :: work
+		use mpi
+		use matrix_interface
 
-        
-        lwork = 100000
-        lrwork = 4*size(w) -2 ! dopo proviamo 
-
-        allocate(work(lwork))
-
-        allocate(rwork(lrwork))
-
-        CALL pzheev( 'V', 'U', size(w) , A, 1,1, DESCA, W, Z, 1, 1, DESCZ, WORK, LWORK, RWORK, LRWORK, INFO )
-	
-        deallocate(work)
-        deallocate(rwork)
-
-
-end subroutine
-!------------------------------------------------------------------------------------------
-
-!diagonalize matrix
-!it doesn't work
-subroutine ddzm2(A, desca, Z, descz, W)
-        
-        use matrix_interface
-        implicit none
-        
-        !input 
-        double complex , dimension(:,:), intent(INOUT) :: A, Z
-        integer , dimension(:), intent(INOUT):: desca, descz
-        double precision, dimension(:), intent(INOUT)::W
-        double complex, dimension(:), allocatable ::  rwork
-        integer :: lwork, lrwork,liwork
-        integer :: info
-        double precision, dimension(:), allocatable :: work
-	integer, dimension(:), allocatable :: iwork
-	
-        
-        lwork = 100000
-        lrwork = 30000!4*size(w) -2 ! dopo proviamo 
-        liwork = 1000
-        allocate(work(lwork))
-	allocate(iwork(liwork))
-        allocate(rwork(lrwork))
-
-        CALL pzheevr( 'V','A' ,'U', size(w) , A, 1,1, DESCA,&
-        -100.d0,100.d0,0,100,size(w),size(w) ,W, Z, 1, 1, DESCZ, WORK, LWORK, &
-        	 RWORK, LRWORK,IWORK,LIWORK, INFO )
-	
-	 
-        deallocate(work)
-        deallocate(rwork)
-
-
-end subroutine
-!-----------
-
-
-!matmul
-subroutine dmatmul(A,descA,B,descB,C,descC)
-	use mpi 
-	implicit none
-        double complex, dimension(:,:), intent(INOUT) :: A, B, C
-        integer, dimension(:), intent(INOUT) :: descA, descB, descC 
- 
-        
-        if (descA(4) .ne. descB(3)) then 
-                write(*,*) "invalid matrix dimensions"
-                STOP
-        end if
-        if (descC(4) .ne. descB(4))  then 
-                 write(*,*) "invalid matrix dimensions"
-                 STOP
-        end if
-        if (descC(3) .ne. descA(3)) then 
-                 write(*,*) "invalid matrix dimensions"
-                 STOP
-        end if
-       
- 	!do ii =0 , 3
-    	!if(iam .eq. ii) then
-    	!print*, iam , ia, ja, ib ,jb
-    	!end if 
-    	!call mpi_barrier(MPI_COMM_WORLD, info) 
-    	!end do
-    	
- 	C = dcmplx(0.d0,0.d0)
-        call pzgemm('N', 'N',descA(3), descB(4), descA(4), dcmplx(1.0,0.0), & 
-        A, 1, 1, descA, B,  1, 1, descB, dcmplx(1.0,0.0), C, 1,1, descC)
-       
-        end subroutine
-!-----------------------------------------------------------
-!Must be tested
-	subroutine distributedvsfull(A,descA, B)
 		implicit none
-		double complex , dimension(:,:) ::A
+		
+		! input
+		double complex , dimension(:,:), intent(INOUT) :: M, A
+		integer, intent(INOUT) ::  iam
+		integer, dimension(:), intent(INOUT) ::  desca
+		! variables
+		integer :: sizeG 
+		integer :: ierr
+		integer :: ii, jj 
+		
+		! Check if M is square
+		if( size(M, dim=1) .eq. size(M, dim=2)) then 
+			sizeG = size(M, dim=1)
+		else 
+			print*, "M is not a square matrix!"
+			STOP
+		end if
+		
+		! Use M from the first process
+		if(iam .eq. 0) then 
+			print*, "IAM = 0!"
+			call MPI_BCAST(m,sizeg*sizeg, MPI_DOUBLE_COMPLEX, 0 ,MPI_COMM_WORLD,ierr)
+		else 
+			call MPI_BCAST(m,sizeg*sizeg, MPI_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD,ierr )
+		end if
+			
+		! Print matrices
+		!do ii = 0, nprows*npcols
+			!if (iam .eq. ii) then
+				!print*, "******"
+				!call pzm(M)
+			!end if 
+			!call mpi_barrier(MPI_COMM_WORLD, ierr) 
+		!end do
+
+		if (ierr .ne. 0) then 
+			print*, "Error building matrix!"
+			STOP
+		end if
+		
+		! Distribute M 
+		DO ii = 1, sizeg
+			DO jj = 1, sizeg
+				CALL PZELSET(A, ii, jj, DESCA, M(ii,jj))	
+			END DO
+		END DO
+
+	end subroutine 
+
+	! Simple interface to print a distributed matrix
+	subroutine printmat(A, descA)
+
+		integer, dimension(:) :: desca 
+		double complex, dimensioN(:,:) :: A
+		double complex, dimension(30) :: work
+
+		CALL PZLAPRNT(descA(3), descA(4), A, 1, 1, DESCA, 0, 0, 'A', 6, WORK)
+
+	end subroutine 
+
+	! Distributed matrix diagonalization 
+	subroutine ddzm(A, desca, Z, descz, W)
+			
+		use matrix_interface
+
+		implicit none
+		
+		double complex, dimension(:,:), intent(INOUT) :: A, Z
+		integer, dimension(:), intent(INOUT) :: desca, descz
+		double precision, dimension(:), intent(INOUT) :: W
+		double complex, dimension(:), allocatable :: rwork
+		integer :: lwork, lrwork
+		integer :: info
+		double precision, dimension(:), allocatable :: work
+
+		lwork = 100000
+		lrwork = 4*size(w) - 2 ! dopo proviamo 
+
+		allocate(work(lwork))
+		allocate(rwork(lrwork))
+
+		CALL pzheev('V', 'U', size(w), A, 1, 1, DESCA, W, Z, 1, 1, DESCZ, WORK, LWORK, RWORK, LRWORK, INFO)
+	
+		deallocate(work)
+		deallocate(rwork)
+
+	end subroutine
+
+	! Distributed matrix multiplication
+	subroutine dmatmul(A, descA, B, descB, C, descC)
+		
+		use mpi 
+
+		implicit none
+
+		double complex, dimension(:,:), intent(INOUT) :: A, B, C
+		integer, dimension(:), intent(INOUT) :: descA, descB, descC 
+
+		if (descA(4) .ne. descB(3)) then 
+			write(*,*) "Invalid matrix dimensions!"
+			STOP
+		end if
+		if (descC(4) .ne. descB(4))  then 
+			write(*,*) "Invalid matrix dimensions!"
+			STOP
+		end if
+		if (descC(3) .ne. descA(3)) then 
+			write(*,*) "Invalid matrix dimensions!"
+			STOP
+		end if
+			
+		C = dcmplx(0.d0,0.d0)
+		
+		call pzgemm('N', 'N', descA(3), descB(4), descA(4), dcmplx(1.0,0.0), & 
+			        & A, 1, 1, descA, B, 1, 1, descB, dcmplx(1.0,0.0), C, 1, 1, descC)
+		
+	end subroutine
+
+	! -----------------------------------------------------------
+	! SUBROUTINES/FUNCTIONS TO BE TESTED OR NOT WORKING
+	! -----------------------------------------------------------
+
+	!diagonalize matrix
+	!it doesn't work
+	subroutine ddzm2(A, desca, Z, descz, W)
+			
+		use matrix_interface
+
+		implicit none
+
+		double complex, dimension(:,:), intent(INOUT) :: A, Z
+		integer, dimension(:), intent(INOUT) :: desca, descz
+		double precision, dimension(:), intent(INOUT) :: W
+		double complex, dimension(:), allocatable :: rwork
+		integer :: lwork, lrwork, liwork
+		integer :: info
+		double precision, dimension(:), allocatable :: work
+		integer, dimension(:), allocatable :: iwork
+
+		lwork = 100000
+		lrwork = 30000 !4*size(w) -2 ! dopo proviamo 
+		liwork = 1000
+
+		allocate(work(lwork))
+		allocate(iwork(liwork))
+		allocate(rwork(lrwork))
+
+		CALL pzheevr('V', 'A', 'U', size(w), A, 1, 1, DESCA, -100.d0, 100.d0, 0, 100, size(w), &
+		             & size(w), W, Z, 1, 1, DESCZ, WORK, LWORK, RWORK, LRWORK, IWORK, LIWORK, INFO)
+
+		deallocate(work)
+		deallocate(rwork)
+
+	end subroutine
+
+	!Must be tested
+	subroutine distributedvsfull(A, descA, B)
+
+		implicit none
+
+		double complex , dimension(:,:) :: A
 		double complex, dimension (:,:), allocatable :: Bprime
-		double complex , dimension(:,:) ::B
+		double complex , dimension(:,:) :: B
 		integer , dimension(:) :: descA
 		integer, dimension(9) :: descB
-		double complex :: alpha,tot
-		integer :: ii , jj 
+		double complex :: alpha, tot
+		integer :: ii, jj 
 		
 		descB = descA
+
 		allocate(Bprime(size(A, dim = 1), size(A, dim= 2)))
+
 		do ii = 1 , descA(3)
 			do jj = 1, descA(4)
-			
-			call pzelset(Bprime, ii ,jj , descB, B(ii,jj))
-			
+				call pzelset(Bprime, ii ,jj , descB, B(ii,jj))
 			end do 
-		end do 
+		end do
+
 		print*, sum(A-Bprime)
 		
 	end subroutine
-	
-!------------------------------------------------------
-	function getcol(A,descA,col) result(res)
 		
-		double complex , dimension(:,:) :: A
+	! Get column
+	function getcol(A, descA, col) result(res)
 		
+		double complex, dimension(:,:) :: A
 		integer:: dimr, dimc, descA(9), col, ii
 		double complex :: alpha
 		!parameter(dimr = descA(3), dimc = descA(4))
-		double complex , dimension(descA(3)) :: res
+		double complex, dimension(descA(3)) :: res
 		
 		do ii = 1, descA(3)
-			
-			call pzelget('A',' ',res(ii),A,ii,col,descA)
-			
+			call pzelget('A', ' ', res(ii), A, ii, col, descA)
 		end do
 		
 		return
+
 	end function
 
-
 end module scalapack_interface
-
-
-
-
